46a88f5f1d184dbe509c6cbd041cbe56d74681ef
Agnieszka Grala <60665472+Swietygraal@users.noreply.github.com>
2024-01-22 11:32:30 +0100

    Funkcje do koszyka i początek panelu admina

--- controllers/shop.js
+++ controllers/shop.js
@@ -8,3 +8,24 @@ exports.getCart = (req, res) => {
     }
     res.render('cart', {cart: cart, total: total});
 };
+
+exports.deleteCartItem = (req, res) => {
+    let cart = req.session.cart;
+    let id = req.body.productID;
+    let i;
+    for (i = 0; i < cart.length; i++) {
+        if (cart[i].ID == id) {
+            break;
+        }
+    }
+    cart.splice(i, 1);
+    res.redirect('/cart');
+};
+
+exports.addCartItem = async (req, res) => {
+    let id = req.body.productID;
+    await req.conn.connect();
+    var product = await req.ProdRepo.retrieve(id);
+    req.session.cart.push(product);
+    //tu jakiś render albo redirect by się przydał
+};
